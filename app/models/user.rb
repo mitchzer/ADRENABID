d5a62a053c012c858a0e5c12efd4bef9ee0992b6
AbelBoot <abellocati@hotmail.com>
2018-06-05 12:29:02 +0100

    add seeds, corrected model and add method in model to create wallet when user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   has_one :wallet
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  after_create :create_user_wallet
+  def create_user_wallet
+    Wallet.create(user: self)
+  end
 end
