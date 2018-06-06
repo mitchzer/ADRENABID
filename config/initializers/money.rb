741da57e84fd656f13c1a726a142359bca02689f
Michel Wlodarczyk <michel.wlodarczyk@edu.escpeurope.eu>
2018-06-06 16:27:23 +0100

    working on the bidding input

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,4 +1,5 @@
 # config/initializers/money.rb
 MoneyRails.configure do |config|
   config.default_currency = :eur  # or :gbp, :usd, etc.
+  config.default_format = { no_cents_if_whole: false }
 end
