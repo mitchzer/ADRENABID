b9e955a0aca0f01639df47e9849db89dea1ef7dc
Michel Wlodarczyk <michel.wlodarczyk@edu.escpeurope.eu>
2018-06-07 11:58:18 +0100

    improving stuff

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,4 +2,6 @@ class Bid < ApplicationRecord
   belongs_to :auction
   belongs_to :user
   monetize :price_cents
+  validates :price_cents, uniqueness: { scope: :user,
+    message: "You have already bid for this price" }
 end
