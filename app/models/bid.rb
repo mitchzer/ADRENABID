2bc5cb884249b41ccce1660fe10ddf9c3663d138
Michel Wlodarczyk <michel.wlodarczyk@edu.escpeurope.eu>
2018-06-07 12:10:14 +0100

    fixing issue

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,6 +2,6 @@ class Bid < ApplicationRecord
   belongs_to :auction
   belongs_to :user
   monetize :price_cents
-  validates :price_cents, uniqueness: { scope: :user,
+  validates :price_cents, uniqueness: { scope: [:user, :auction],
     message: "You have already bid for this price" }
 end
