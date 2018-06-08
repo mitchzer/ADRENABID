936ce132d2872108935eca77a24ff356cef07294
alexanderauer <aauer300@gmail.com>
2018-06-08 14:52:46 +0200

    How it works done and footer works good now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :info]
 
   def home
     render :layout => "home.html.erb"
+  end
+
+  def info
+    render "info.html.erb"
   end
 end
