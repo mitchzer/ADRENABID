5f173827180e82d79be9711fcc0d8a7e0f306290
Michel Wlodarczyk <michel.wlodarczyk@edu.escpeurope.eu>
2018-06-12 10:01:57 +0100

    okay

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,13 +3,14 @@ class Bid < ApplicationRecord
   belongs_to :user
   validates :price_cents, uniqueness: { scope: [:user, :auction],
     message: "You have already bid for this price" }
-  validates :price_cents, numericality: { only_integer: true, message: "Can not be empty" }
+  validates :price_cents, numericality: { only_integer: true, message: "Can not be empty (has to be a price in cents)" }
+  validates :price_cents, numericality: { greater_than: 0, message: "Non valid price" }
   validate :price_cents_is_multiple_of_price_step_cents
   monetize :price_cents
 
   def price_cents_is_multiple_of_price_step_cents
     if price_cents != nil && price_cents % auction.price_step_cents != 0
-      errors.add(:price_cents, "Must be a proper mutliple of price step")
+      errors.add(:price_cents, "Must be a proper mutliple of #{auction.price_step_cents} cents")
     end
   end
 end
